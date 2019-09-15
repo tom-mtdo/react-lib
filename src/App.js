5cfb0fb0c3ce5bb15b1cc1f88a73d44540ba36b7
Tom Do <tom@sisuwellness.com>
2019-09-15 23:59:36 +1000

    test

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import './App.css';
 import ContextExample from './context';
 import StyledComponent from './styled-components';
+import MyHoc from './hoc';
 
 function App() {
   return (
     <>
+      <MyHoc/>
+      <br></br>
       <StyledComponent/>
       <br></br>
       <ContextExample/>
